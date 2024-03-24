ada273756462db2d23921c93b786f441a1378347
Dmitry Kutsenko <dmitrykutsenko@MacBook-Pro-Dmitry.local>
2024-03-24 13:09:21 +0300

    Update bot

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,35 +1,66 @@
 import aiogram
 from aiogram import types, Dispatcher
-from stickerify import stickerify
+from stickerify import stickerify, pipelines_dict
 from PIL import Image
 from io import BytesIO
 
-user_sticker_packs = {}
+photo_storage = {}
+
+
+def get_styles_markup():
+    markup = types.InlineKeyboardMarkup()
+    default_btn = types.InlineKeyboardButton("Default 🤫🧏‍", callback_data="default")
+    flowers_btn = types.InlineKeyboardButton("Flowers 🌸🌺", callback_data="flowers")
+    markup.add(default_btn, flowers_btn)
+    return markup
 
 
 async def handle_start(message: types.Message):
-    user_id = message.from_user.id
-    await message.reply("Welcome to Stickerify bot!")
+    await message.reply("Welcome to Stickerify bot! 🥶\nSend me a photo and I will create your own sticker 👠")
 
 
 async def handle_photo(message: types.Message, bot):
     photo = message.photo[-1]
+    file_id = photo.file_id
+    chat_id = message.chat.id
+    photo_storage[chat_id] = file_id
 
-    file = await bot.get_file(photo.file_id)
-    file_path = file.file_path
-    contents = await bot.download_file(file_path)
+    await message.reply("Choose your sticker style:", reply_markup=get_styles_markup())
 
-    image = Image.open(BytesIO(contents.getvalue()))
-    stickerified_image = stickerify(image)
 
-    bio = BytesIO()
-    bio.name = 'image.jpeg'
-    stickerified_image.save(bio, 'JPEG')
-    bio.seek(0)
+async def process_stickerify_callback(callback_query: types.CallbackQuery, bot):
+    chat_id = callback_query.from_user.id
+    sticker_style = callback_query.data
+    if chat_id in photo_storage:
+        file_id = photo_storage[chat_id]
+        try:
+            file = await bot.get_file(file_id)
+            file_path = file.file_path
+            contents = await bot.download_file(file_path)
 
-    # Send the processed image back to the user
-    await bot.send_photo(chat_id=message.chat.id, photo=bio, caption="Here is your sticker")
+            await bot.send_message(chat_id, "Started generating your sticker! 👨‍🔬")
+
+            image = Image.open(BytesIO(contents.getvalue()))
+            stickerified_image = stickerify(image, sticker_style)
+
+            bio = BytesIO()
+            bio.name = "image.jpeg"
+            stickerified_image.save(bio, "JPEG")
+            bio.seek(0)
+
+            await bot.send_photo(chat_id, photo=bio, caption="Here is your sticker! 🎁")
+
+            del photo_storage[chat_id]
+
+        except Exception as e:
+            await bot.send_message(chat_id, f"Sorry, an error occurred.")
+
+    else:
+        await bot.send_message(chat_id, "I couldn't find your photo. Please send it again.")
+
 
 def setup_handlers(dp: Dispatcher, bot):
     dp.register_message_handler(handle_start, commands=['start'])
     dp.register_message_handler(lambda message: handle_photo(message, bot), content_types=['photo'])
+    dp.register_callback_query_handler(lambda query: process_stickerify_callback(query, bot),
+                                       lambda query: query.data in pipelines_dict.keys())
--- bot/stickerify.py
+++ bot/stickerify.py
@@ -1,11 +1,12 @@
 import torch
 from diffusers import (AutoencoderKL, DDPMScheduler,
                        StableDiffusionInstructPix2PixPipeline,
-                       UNet2DConditionModel)
+                       UNet2DConditionModel, DiffusionPipeline)
 from diffusers.utils import load_image
 from PIL import Image
 
 model_id = "Alexator26/instructPix2PixCartoon_700imgs_train"
+model_flowers_id = "misshimichka/pix2pix_people_flowers_v2"
 
 pipeline = StableDiffusionInstructPix2PixPipeline.from_pretrained(
     model_id, torch_dtype=torch.float16, safety_checker=None).to("mps")
@@ -20,10 +21,18 @@ def load_unet(pipeline, idx):
     return pipeline.to("mps")
 
 
-def stickerify(original_image):
+pipelines_dict = {
+    "default": 0,
+    "flowers": 1
+}
+
+
+def stickerify(original_image, mode):
     original_image = original_image.resize((512, 512))
     generator = torch.Generator(device='mps').manual_seed(42)
-    edited_image = pipeline("Refashion the photo into a sticker.",
+    assert mode in pipelines_dict.keys()
+    edited_image = pipelines[pipelines_dict[mode]](
+                            "Refashion the photo into a sticker.",
                             image=original_image,
                             num_inference_steps=20,
                             image_guidance_scale=1.5,
@@ -32,4 +41,8 @@ def stickerify(original_image):
     return edited_image
 
 
-pipeline = load_unet(pipeline, 4860)
+pipelines = [load_unet(pipeline, 4860),
+             DiffusionPipeline.from_pretrained(model_flowers_id,
+                                               torch_dtype=torch.float16,
+                                               safety_checker=None).to("mps")
+             ]
